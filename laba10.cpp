ce64435c17fe23d5125aa3cd6712fde13d8e792f
janosBacsi <ivan13oct@gmail.com>
2020-03-18 22:58:39 +0200

    modified:   Debug/books.obj 	modified:   Debug/laba10.log 	modified:   Debug/laba10.obj 	modified:   Debug/laba10.tlog/CL.command.1.tlog 	modified:   Debug/laba10.tlog/CL.read.1.tlog 	modified:   Debug/laba10.tlog/CL.write.1.tlog 	modified:   Debug/laba10.tlog/link.read.1.tlog 	modified:   Debug/vc142.idb 	modified:   Debug/vc142.pdb 	modified:   books.cpp 	modified:   books.h 	modified:   input.txt 	modified:   laba10.cpp

--- laba10.cpp
+++ laba10.cpp
@@ -6,33 +6,30 @@
 
 #include "books.h"
 
-
-int main()
-{
-	struct sBooks* pCurrentBookPointer =NULL;
+int main() {
+	struct sBooks* pCurrentBookPointer = NULL;
 	char cInputFileAddress[] = "input.txt";
-	if (readFromFile(cInputFileAddress, &pCurrentBookPointer) == 0)
-	{
+	if (readFromFile(cInputFileAddress, &pCurrentBookPointer) == 0) {
 		return 0;
 	}
-	
+
 	HWND console = GetConsoleWindow();
 	RECT r;
-	GetWindowRect(console, &r); //stores the console's current dimensions
+	GetWindowRect(console, &r); 
 
 	MoveWindow(console, r.left, r.top, 1300, 600, TRUE);
 	int iUserAnswer = 3;
-	printf("Book store management software, by Chaus Ivan ");
+	printf("Book store management software, by Chaus Ivan \n");
 
-	while (iUserAnswer != 0)
-	{
-		printf("\t(1)Add"
+	while (iUserAnswer != 0) {
+		printf(
+			"\t(1)Add new element"
 			"\n\t(2)Sort List"
 			"\n\t(3)Print list"
-			"\n\t(4)Print books with price less than certain price"
+			"\n\t(4)Print books with price less than a certain filter"
 			"\n\t(5)Write to file"
-			"\n\t(6)Delete a single element"
-			"\n\t(7)Save   "
+			"\n\t(6)Delete an element by index"
+			"\n\t(7)Save changes"
 			"\n\t(8)Search by author's last name"
 			"\n\t(9)Print two books with the most pages  "
 			"\n\t(10)Edit existing entry"
@@ -40,45 +37,54 @@ int main()
 
 		iUserAnswer = readFoolProofInt();
 		MoveWindow(console, r.left, r.top, 1000, 600, TRUE);
-		switch (iUserAnswer)
-		{
-		case 1:
+		switch (iUserAnswer) {
+		case 1: {
 			addNewNode(&pCurrentBookPointer);
 			break;
-		case 2:
+		}
+		case 2: {
 			sortList(pCurrentBookPointer);
 			printf("\nList successfully sorted by author's last name\n\n");
 			break;
-		case 3:
+		}
+		case 3: {
 			printList(pCurrentBookPointer);
 			break;
-		case 4:
+		}
+		case 4: {
 			lessThanCertainPrice(&pCurrentBookPointer);
 			break;
-		case 5:
+		}
+		case 5: {
 			writeToFile(pCurrentBookPointer);
 			break;
-		case 6:
+		}
+		case 6: {
 			deleteSingleElement(&pCurrentBookPointer);
 			break;
-		case 7:
+		}
+		case 7: {
 			saveChanges(pCurrentBookPointer);
 			break;
-		case 8:
+		}
+		case 8: {
 			searchByAuthorsLastName(&pCurrentBookPointer);
 			break;
+		}
 		case 9:
 			booksWithTheMostPages(pCurrentBookPointer);
 			break;
-		case 10:
+		case 10: {
 			editElementByIndex(pCurrentBookPointer);
 			break;
-		case 0:
+		}
+		case 0: {
 			return 0;
 			break;
-		default:
+		}
+		default: {
 			printf("\nYou have entered the wrong number. Please try again\n");
-
+		}
 		}
 	}
 }
